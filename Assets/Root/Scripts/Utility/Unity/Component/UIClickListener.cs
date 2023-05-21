79e4933dbc2aa7f32f8660d7b83673ea912f7734
Yoziya <1500299119@qq.com>
2023-05-21 22:41:54 +0800

    新增组件

---
+++ Assets/Root/Scripts/Utility/Unity/Component/UIClickListener.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Yoziya
+{
+    public class UIClickListener : UIBehaviour, IPointerClickHandler
+    {
+        [System.Serializable]
+        public class ClickWorldEvent : UnityEvent<Vector3> { }
+
+        [System.Serializable]
+        public class ClickCanvasEvent : UnityEvent<Vector2> { }
+
+        public ClickWorldEvent OnWorldClicked;
+        public ClickCanvasEvent OnCanvasClicked;
+        private RectTransform mTransform;
+
+        protected override void Start()
+        {
+            mTransform = GetComponent<RectTransform>();
+            OnWorldClicked.AddListener(position => { Debug.Log(position); });
+            OnCanvasClicked.AddListener(position => { Debug.Log(position.x); });
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            OnWorldClicked?.Invoke(eventData.position);
+            Vector2 canvasPosition = ConvertToCanvasPosition(eventData.position);
+            OnCanvasClicked?.Invoke(canvasPosition);
+        }
+
+        private Vector2 ConvertToCanvasPosition(Vector3 worldPosition)
+        {
+            Vector2 localPoint = new Vector2(
+                worldPosition.x / mTransform.rect.width,
+                worldPosition.y / mTransform.rect.height
+            );
+            return localPoint;
+        }
+    }
+}
